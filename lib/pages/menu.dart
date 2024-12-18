b6b99ae5b1c2fe25bcd7b089fcbcdcc840c26447
vaishnavagopinathbhat <vaishnavbhat996@gmail.com>
2024-12-18 14:59:27 +0530

    image_picker and staggered_gridview usage

---
+++ lib/pages/menu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // actions: [
+        //   IconButton(
+        //       onPressed: () {
+        //         Navigator.pop(context);
+        //       },
+        //       icon: const Icon(Icons.arrow_back))
+        // ],
+      ),
+    );
+  }
+}
